f6c37d24095ba77abedcb1388e7c5c5abe3fb4cb
benpoulter99 <benpoulter99@hotmail.com>
2019-01-24 16:25:39 +0000

    Created Buttons

--- meetingSchedular/Form1.Designer.cs
+++ meetingSchedular/Form1.Designer.cs
@@ -28,25 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.MeetingScheduler = new System.Windows.Forms.Button();
+            this.UpdateMeeting = new System.Windows.Forms.Button();
+            this.CancelMeeting = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // MeetingScheduler
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(236, 85);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Book Meeting";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.MeetingScheduler.Location = new System.Drawing.Point(12, 12);
+            this.MeetingScheduler.Name = "MeetingScheduler";
+            this.MeetingScheduler.Size = new System.Drawing.Size(236, 85);
+            this.MeetingScheduler.TabIndex = 0;
+            this.MeetingScheduler.Text = "Book Meeting";
+            this.MeetingScheduler.UseVisualStyleBackColor = true;
+            this.MeetingScheduler.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UpdateMeeting
+            // 
+            this.UpdateMeeting.Location = new System.Drawing.Point(12, 103);
+            this.UpdateMeeting.Name = "UpdateMeeting";
+            this.UpdateMeeting.Size = new System.Drawing.Size(236, 90);
+            this.UpdateMeeting.TabIndex = 1;
+            this.UpdateMeeting.Text = "Re-Plan Meeting";
+            this.UpdateMeeting.UseVisualStyleBackColor = true;
+            // 
+            // CancelMeeting
+            // 
+            this.CancelMeeting.Location = new System.Drawing.Point(12, 199);
+            this.CancelMeeting.Name = "CancelMeeting";
+            this.CancelMeeting.Size = new System.Drawing.Size(236, 90);
+            this.CancelMeeting.TabIndex = 2;
+            this.CancelMeeting.Text = "Cancel Meeting";
+            this.CancelMeeting.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1139, 792);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CancelMeeting);
+            this.Controls.Add(this.UpdateMeeting);
+            this.Controls.Add(this.MeetingScheduler);
             this.Name = "Form1";
             this.Text = "Meeting Scheduler";
             this.ResumeLayout(false);
@@ -55,8 +77,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button bookMeeting;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button MeetingScheduler;
+        private System.Windows.Forms.Button UpdateMeeting;
+        private System.Windows.Forms.Button CancelMeeting;
+
     }
 }
 
